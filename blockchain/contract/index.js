81f2e75482e498d6abb6adf8ed0ddd56730494d0
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-05-01 23:16:18 +0200

    Updating index.js

--- blockchain/contract/index.js
+++ blockchain/contract/index.js
@@ -4,14 +4,18 @@
 
 'use strict';
 
-const DataContract = require('./lib/DataContract');
 
-//const AccessToken = require('./lib/AccessTokenContract');
-//const Data = require('./lib/DataContract');
-//const Buyer = require('./lib/user/BuyerContract');
-//const Seller = require('./lib/user/SellerContract');
-//const Observer = require('./lib/user/ObserverContract');
-const UserContract = require('./lib/UserContract');
+const AccessTokens = require('./lib/AccessTokenContract');
+const Data = require('./lib/DataContract');
+const Buyers = require('./lib/user/BuyerContract');
+const Sellers = require('./lib/user/SellerContract');
+const Observers = require('./lib/user/ObserverContract');
+const Users = require('./lib/UserContract');
 
-module.exports.UserContract = UserContract;
-module.exports.contracts = [ DataContract, UserContract ];
+module.exports.AccessTokens = AccessTokens;
+module.exports.Data = Data;
+module.exports.Buyers = Buyers;
+module.exports.Sellers = Sellers;
+module.exports.Observers = Observers;
+module.exports.Users = Users;
+module.exports.contracts = [ AccessTokens, Data, Buyers, Sellers, Observers, Users ];
